474962dac5d316544e34cba58d6e0374abf353b4
Moritz <moritz.vogt@vogges.de>
2022-11-01 10:12:53 +0100

    refactor: Ad comments to the base Service

--- app/main/services/base.py
+++ app/main/services/base.py
@@ -14,20 +14,46 @@ UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
 
 class BaseService(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
     def __init__(self, model: Type[ModelType], db_session: Session):
+        """
+            Base Service for CRUD Operation´s like READ, UPDATE, DELETE AND CREATE
+
+
+        :param model: model which is used.
+        :param db_session: db session for database connection
+        """
         self.model = model
         self.db_session = db_session
 
-    def get(self, id: Any) -> Optional[ModelType]:
-        obj: Optional[ModelType] = self.db_session.query(self.model).get(id)
+    def get(self, object_id: Any) -> Optional[ModelType]:
+        """
+        Get´s a specific object from the database
+
+
+        :param object_id: Identifier for the object, which is going to be searched.
+        :return: Found Object
+        """
+        obj: Optional[ModelType] = self.db_session.query(self.model).get(object_id)
         if obj is None:
             raise HTTPException(status_code=404, detail="Not Found")
         return obj
 
     def list(self) -> List[ModelType]:
+        """
+        Query´s everything for the specific Object Type
+
+        :return: List of Objects
+        """
         objs: List[ModelType] = self.db_session.query(self.model).all()
         return objs
 
     def create(self, obj: CreateSchemaType) -> ModelType:
+        """
+        Create´s an object in the database.
+
+
+        :param obj: Object which is going to be created in the database.
+        :return: Created Object
+        """
         db_obj: ModelType = self.model(**obj.dict())
         self.db_session.add(db_obj)
         try:
@@ -40,14 +66,28 @@ class BaseService(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
                 raise e
         return db_obj
 
-    def update(self, id: Any, obj: UpdateSchemaType) -> Optional[ModelType]:
-        db_obj = self.get(id)
+    def update(self, object_id: Any, obj: UpdateSchemaType) -> Optional[ModelType]:
+        """
+        Update´s a specific Object in the database.
+
+        :param object_id: Identifier of the object which is going to be updated
+        :param obj: Data of the updated Object
+        :return: Updated Object.
+        """
+        db_obj = self.get(object_id)
         for column, value in obj.dict(exclude_unset=True).items():
             setattr(db_obj, column, value)
         self.db_session.commit()
         return db_obj
 
-    def delete(self, id: Any) -> None:
-        db_obj = self.db_session.query(self.model).get(id)
+    def delete(self, object_id: Any) -> None:
+        """
+        Delete´s a specific Object by its Identifier
+
+        :param object_id: Identifier of the Object which is going to be deleted
+        :return:
+        """
+
+        db_obj = self.db_session.query(self.model).get(object_id)
         self.db_session.delete(db_obj)
         self.db_session.commit()
