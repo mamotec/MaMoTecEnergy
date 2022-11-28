26fa63a06c724db0f3581cf90ccf2153a89bde1e
Moritz <moritz.vogt@vogges.de>
2022-11-28 14:27:22 +0100

    Implement alembic

--- app/main/database/base.py
+++ app/main/database/base.py
@@ -1,4 +1,4 @@
 # Import all the models, so that Base has them before being
 # imported by Alembic
 from app.main.database.db import Base  # noqa
-from app.main.database.models.register import Register  # noqa
+from app.main.database.models.port import Port  # noqa
